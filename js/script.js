f3604c5262b4c6a24107dc2c0d493ec2cdd6a0b2
AsifAlishishir <asifali210084@diit.edu.bd>
2025-08-28 01:48:49 +0600

    added call button class

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// HEART COUNT EVENT
 const heartCountBtns = document.querySelectorAll(".small-heart-btn");
 const heartTotalElement = document.getElementById("heart-Total");
 let heartTotal = parseInt(heartTotalElement.innerText);
@@ -6,5 +7,13 @@ for (const heartBtn of heartCountBtns) {
   heartBtn.addEventListener("click", function () {
     heartTotal++;
     heartTotalElement.innerText = heartTotal;
+  });
+}
+
+// CALL EVENT
+const callCountBtns = document.querySelectorAll(".call-btn");
+for (const callBtn of callCountBtns) {
+  callBtn.addEventListener("click", function () {
+    alert('Calling');
   });
 }
