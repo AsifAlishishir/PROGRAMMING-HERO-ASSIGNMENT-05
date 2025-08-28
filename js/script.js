4b4e4fc830fabf855f973006f6b01cc97636ba1e
AsifAlishishir <asifali210084@diit.edu.bd>
2025-08-28 16:17:25 +0600

    added the function of when clickin call the name,number and local time will show in the call history and added the copy event and the copy to clipboard part also using clipboard-writetext() function

--- js/script.js
+++ js/script.js
@@ -19,10 +19,12 @@ for (const heartBtn of heartCountBtns) {
 // }
 
 // CALL EVENT
+
 const cardSection = document.getElementsByClassName("card-service");
 const coinCount = document.getElementById("coin-count");
 let coinTotal = parseInt(coinCount.innerText);
-console.log(coinTotal);
+const historyCard = document.getElementById("history-card");
+
 for (const card of cardSection) {
   const cardH2 = card.querySelector("h2").innerText;
   const cardH3 = card.querySelector("h3").innerText;
@@ -37,5 +39,50 @@ for (const card of cardSection) {
 
     coinTotal -= 20;
     coinCount.innerText = coinTotal;
+
+    const call = {
+      serviceName: cardH2,
+      serviceNumber: cardH3,
+      date: new Date().toLocaleTimeString(),
+    };
+
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div
+              class="call-history-content p-4 mx-6 mb-3 flex justify-between items-center bg-[#FAFAFA] rounded-lg"
+            >
+              <div class="history-description">
+                <h2 class="inter text-[18px] font-semibold">
+                  ${call.serviceName}
+                </h2>
+                <p class="hind text-[18px] text-[#5C5C5C] font-normal">${call.serviceNumber}</p>
+              </div>
+              <div class="call-history-date hind text-[18px] font-normal">
+                <p>${call.date}</p>
+              </div>
+            </div>`;
+    historyCard.appendChild(div);
+  });
+}
+
+// CLEAR BUTTON EVENT
+document.getElementById("clear-btn").addEventListener("click", function () {
+  const historyCard = document.getElementById("history-card");
+  historyCard.innerHTML = ``;
+});
+
+// COPY BUTTON EVENT
+const copyCount = document.getElementById("copy-count-total");
+let copyTotal = parseInt(copyCount.innerText);
+
+for (const card of cardSection) {
+  const cardH3 = card.querySelector("h3").innerText;
+  const copyBtn = card.querySelector(".copy-btn");
+  copyBtn.addEventListener("click", function () {
+    navigator.clipboard.writeText(cardH3);
+    alert("Number has been copied: " + cardH3);
+
+    copyTotal++;
+    copyCount.innerText = copyTotal;
   });
 }
