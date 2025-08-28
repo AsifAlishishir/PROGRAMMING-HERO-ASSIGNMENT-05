80632b758738a7900e74b225060623c3d7b879fe
AsifAlishishir <asifali210084@diit.edu.bd>
2025-08-28 12:30:53 +0600

    added hover effect on buttons and add call button event and alert

--- js/script.js
+++ js/script.js
@@ -11,9 +11,31 @@ for (const heartBtn of heartCountBtns) {
 }
 
 // CALL EVENT
-const callCountBtns = document.querySelectorAll(".call-btn");
-for (const callBtn of callCountBtns) {
+// const callCountBtns = document.querySelectorAll(".call-btn");
+// for (const callBtn of callCountBtns) {
+//   callBtn.addEventListener("click", function () {
+
+//   });
+// }
+
+// CALL EVENT
+const cardSection = document.getElementsByClassName("card-service");
+const coinCount = document.getElementById("coin-count");
+let coinTotal = parseInt(coinCount.innerText);
+console.log(coinTotal);
+for (const card of cardSection) {
+  const cardH2 = card.querySelector("h2").innerText;
+  const cardH3 = card.querySelector("h3").innerText;
+
+  const callBtn = card.querySelector(".call-btn");
   callBtn.addEventListener("click", function () {
-    alert('Calling');
+    if (coinTotal < 20) {
+      alert("❌ insufficient Coin! You need 20 Coin to make a call.");
+      return;
+    }
+    alert("📞 Calling " + cardH2 + " " + cardH3 + " ...");
+
+    coinTotal -= 20;
+    coinCount.innerText = coinTotal;
   });
 }
